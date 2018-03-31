b212727d1dd36d91958c419c86ca414d6e1d8c03
akat32 <baduk32127@gmail.com>
2018-04-01 03:49:29 +0900

    undo

--- routes/models/input.js
+++ routes/models/input.js
@@ -25,7 +25,7 @@ module.exports = (router, Group, Solo, FCM)=>{
       "subject" : req.body.subject,
       "question" : req.body.question,
       "name" : req.body.name,
-      "answer" : req.body.answer,
+      "answer" : req.body.answer
     }},"$set":{"num": like}})
     res.send("success");
   })
