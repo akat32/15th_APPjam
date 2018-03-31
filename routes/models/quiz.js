271cfe471fd2399fa14a7ceacd0208d0b6759dd3
akat32 <baduk32127@gmail.com>
2018-04-01 08:14:42 +0900

    sfsadaD

--- routes/models/quiz.js
+++ routes/models/quiz.js
@@ -3,9 +3,7 @@ module.exports = (router, Group, Solo)=>{
     var pan = await Group.findOne({"group_id":req.body.group_id})
     var arr = new Array()
     arr = arr.concat(arr,pan.multiple_choice[1]);
-    arr = arr.concat(arr,pan.short_answer[3]);
     res.status(200).send(arr);
-    console.log(arr[2])
   })
   .post('/solo', async (req,res)=>{
     var pan = await Solo.findOne({"group_id":req.body.group_id})
